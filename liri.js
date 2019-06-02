47f4d2b788a108289c526571b60de320148534be
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-06-02 15:43:55 -0400

    added axios and omdb

--- liri.js
+++ liri.js
@@ -1,25 +1,26 @@
 //setting up environment
 require("dotenv").config();
-
+var fs = require("fs");
 var keys = require("./keys");
 var Spotify = require("spotify-web-api-js");
 var spotify = new Spotify(keys.spotify);
-
+var axios = require("axios");
 var inquirer = require("inquirer");
 
 //variables
-var doInput;
-var whatInput;
-var song;
+var doInput = process.argv[2];
+var whatInput =process.argv[3];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + whatInput + "&y=&plot=short&apikey=trilogy";
 //functions
-var spotifyThat = function(song){
+var spotifyThat = function(whatInput){
     if(whatInput=== undefined){
         whatInput = "Hey I don't know";
     }
     spotify.search(
         {
             type:"track",
-            query: song
+            query: whatInput
         },
         function(err, data){
             if(err){
@@ -29,28 +30,44 @@ var spotifyThat = function(song){
             var info = data.tracks.items;
             for(var i =0; i < info.length; i++){
                 console.log(i);
+                console.log("Song name: "+info[i].name);
             }
         }
     )
 }
+var getMovie = function(){
+    axios.get(queryUrl).then(function(response){
+        var year = response.data.Year;
+        console.log(whatInput+" was released in "+year);
+    })
+}
 
-inquirer.prompt([
-    {
-        name: "what",
-        type:"input",
-        message:"what do you want to do?",
+console.log(doInput, whatInput);
+if(doInput === "spotify"){
+    
+    spotifyThat(whatInput);
+}
+
+if(doInput === "omdb"){
+    getMovie(whatInput);
+}
+// nquirer.prompt([
+//     {
+//         name: "what",
+//         type:"input",
+//         message:"what do you want to do?",
         
-    },
-    {
-        name:"song-movie",
-        type: "input",
-        message: "What should I look up?"
-    }
+//     },
+//     {
+//         name:"song-movie",
+//         type: "input",
+//         message: "What should I look up?"
+//     }
 
-]).then(function(answer){
-    var doInput = answer.name;
-    if(doInput ==="spotify"){
-        spotifyThat();
+// ]).then(function(answer){
+//     var doInput = answer.name;
+//     if(doInput ==="spotify"){
+//         spotifyThat();
 
-    }
-})
+//     }
+// })
