f2946bcd40d2e541873d8c662e08f92be0c053b4
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-06-02 13:47:25 -0400

    node module

--- liri.js
+++ liri.js
@@ -3,3 +3,14 @@ require("dotenv").config();
 var keys = require("./keys");
 
 var spotify = new Spotify(keys.spotify);
+
+var inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        name: "name",
+        message:"whats your name?"
+    }
+]).then(function(answer){
+    console.log(answer.name);
+})
