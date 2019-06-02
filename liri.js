1065bb4ec48d3eea31d5e61f8a39024fb875b818
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-06-02 14:59:19 -0400

    added spotify api

--- liri.js
+++ liri.js
@@ -1,16 +1,56 @@
+//setting up environment
 require("dotenv").config();
 
 var keys = require("./keys");
-
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require("spotify-web-api-js");
+var spotify = new Spotify(keys.spotify);
 
 var inquirer = require("inquirer");
 
+//variables
+var doInput;
+var whatInput;
+var song;
+//functions
+var spotifyThat = function(song){
+    if(whatInput=== undefined){
+        whatInput = "Hey I don't know";
+    }
+    spotify.search(
+        {
+            type:"track",
+            query: song
+        },
+        function(err, data){
+            if(err){
+                console.log("Error: "+err);
+                return;
+            }
+            var info = data.tracks.items;
+            for(var i =0; i < info.length; i++){
+                console.log(i);
+            }
+        }
+    )
+}
+
 inquirer.prompt([
     {
-        name: "name",
-        message:"whats your name?"
+        name: "what",
+        type:"input",
+        message:"what do you want to do?",
+        
+    },
+    {
+        name:"song-movie",
+        type: "input",
+        message: "What should I look up?"
     }
+
 ]).then(function(answer){
-    console.log(answer.name);
+    var doInput = answer.name;
+    if(doInput ==="spotify"){
+        spotifyThat();
+
+    }
 })
